331ab0619a362bd51d97b5ef99beb561350be089
Rodrigo Lisita Ribera <rodrigolisita@gmail.com>
2024-10-09 18:45:03 -0300

    Finalizando o CRUD com opção para obter matriculas de determinado estudante

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -12,7 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Pessoa.hasMany(models.Curso, {foreignKey: 'docente_id'});
-      Pessoa.hasMany(models.Matricula, {foreignKey: 'estudante_id'});
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        scope: {status: 'matriculado'},
+        as: 'aulasMatriculadas'
+      });
     }
   }
   Pessoa.init({
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req,res));
 router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req,res));
-// router.get('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.pegaMatriculasEstudante(req,res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req,res));
 router.post('/pessoas', (req, res) => pessoaController.criaNovo(req,res));
 router.put('/pessoas/:id', (req,res) => pessoaController.atualiza(req,res));
 router.delete('/pessoas/:id', (req,res) => pessoaController.exclui(req,res));
